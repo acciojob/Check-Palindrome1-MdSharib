554404907ce0c6c49bc99ac54827c2458019ae64
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-06-20 12:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
-
+  let cropStr = "";
+  for(let i=0; i<str.length; i++){
+	  if(str[i] === ' '){
+		  continue;
+	  }
+	 cropStr += str[i];
+  }
+  let j= cropStr.length-1;
+  let i = 0;
+  while(i<j){
+    if(cropStr[i] !== cropStr[j]){
+      return false;
+    }
+    j--;
+    i++;
+  }
+	return true;
+	
 }
 module.exports = palindrome
